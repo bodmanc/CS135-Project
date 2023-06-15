b74feece161367eb840474195ce10a71544d564e
Cooper Bodman <cooperjack23@gmail.com>
2023-06-15 02:03:12 -0700

    added forms to project along with better structure for project with dependency injection and get post method, and a way to output posts to list form

--- BikeMates/src/app/app.module.ts
+++ BikeMates/src/app/app.module.ts
@@ -5,17 +5,21 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    PostListComponent,
     HeaderComponent
   ],
   imports: [
@@ -26,7 +30,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
